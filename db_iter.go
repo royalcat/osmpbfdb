ff9da3aff883e96f301f9422ad7a0a7981cef2c3
royalcat <k.adamovich20@gmail.com>
2025-07-24 20:34:07 +0400

    iter nodes and ways

--- db_iter.go
+++ db_iter.go
@@ -28,3 +28,49 @@ func (db *DB) IterRelations() iter.Seq2[*osm.Relation, error] {
 		}
 	}
 }
+
+func (db *DB) IterNodes() iter.Seq2[*osm.Node, error] {
+	return func(yield func(*osm.Node, error) bool) {
+		for _, window := range db.nodeIndex.data {
+			objects, err := db.readObjects(int64(window.v))
+			if err != nil {
+				if !yield(nil, err) {
+					return
+				}
+				continue
+			}
+
+			for _, obj := range objects {
+				if node, ok := obj.(*osm.Node); ok {
+					if !yield(node, nil) {
+						return
+					}
+				}
+			}
+
+		}
+	}
+}
+
+func (db *DB) IterWays() iter.Seq2[*osm.Way, error] {
+	return func(yield func(*osm.Way, error) bool) {
+		for _, window := range db.wayIndex.data {
+			objects, err := db.readObjects(int64(window.v))
+			if err != nil {
+				if !yield(nil, err) {
+					return
+				}
+				continue
+			}
+
+			for _, obj := range objects {
+				if way, ok := obj.(*osm.Way); ok {
+					if !yield(way, nil) {
+						return
+					}
+				}
+			}
+
+		}
+	}
+}
