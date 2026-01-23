be980a2eecbbdda52db30ac7adb9995aac9ff217
github-action-benchmark <github@users.noreply.github.com>
2026-01-23 08:20:02 +0000

    add Benchmark (go) benchmark result for 2e2395efdc34dcd4b2fba63d2ef725fc8a02ea8e

--- bench/branch/ubuntu-24.04/cgo-1/default/data.js
+++ bench/branch/ubuntu-24.04/cgo-1/default/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769154031678,
+  "lastUpdate": 1769156402891,
   "repoUrl": "https://github.com/royalcat/osmpbfdb",
   "entries": {
     "Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 8634,
             "unit": "allocs/op",
             "extra": "4818 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "committer": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "distinct": true,
+          "id": "2e2395efdc34dcd4b2fba63d2ef725fc8a02ea8e",
+          "message": "Merge branch 'continuous-benchmarking'",
+          "timestamp": "2026-01-23T12:18:29+04:00",
+          "tree_id": "4d993223e48b9e56d3ac1d16aed8d508f3fa0ee6",
+          "url": "https://github.com/royalcat/osmpbfdb/commit/2e2395efdc34dcd4b2fba63d2ef725fc8a02ea8e"
+        },
+        "date": 1769156402000,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGet",
+            "value": 2319517,
+            "unit": "ns/op\t 4039534 B/op\t    8660 allocs/op",
+            "extra": "6223 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - ns/op",
+            "value": 2319517,
+            "unit": "ns/op",
+            "extra": "6223 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - B/op",
+            "value": 4039534,
+            "unit": "B/op",
+            "extra": "6223 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - allocs/op",
+            "value": 8660,
+            "unit": "allocs/op",
+            "extra": "6223 times\n4 procs"
           }
         ]
       }
