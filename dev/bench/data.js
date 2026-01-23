fcdb2d6ba77177c87a44a3f669d0ce8b13df1e9b
github-action-benchmark <github@users.noreply.github.com>
2026-01-23 04:37:13 +0000

    add Benchmark (go) benchmark result for 42837faecf3e633944a7b763af03fedda16674bb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769142441728,
+  "lastUpdate": 1769143033287,
   "repoUrl": "https://github.com/royalcat/osmpbfdb",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,54 @@ window.BENCHMARK_DATA = {
             "value": 1695,
             "unit": "ns/op",
             "extra": "679814 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "committer": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "distinct": true,
+          "id": "42837faecf3e633944a7b763af03fedda16674bb",
+          "message": "bench mem",
+          "timestamp": "2026-01-23T08:36:29+04:00",
+          "tree_id": "241cdbddb2325f23ee31303c4e38d3e63f470957",
+          "url": "https://github.com/royalcat/osmpbfdb/commit/42837faecf3e633944a7b763af03fedda16674bb"
+        },
+        "date": 1769143032575,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGet",
+            "value": 1774,
+            "unit": "ns/op\t     112 B/op\t       1 allocs/op",
+            "extra": "610581 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - ns/op",
+            "value": 1774,
+            "unit": "ns/op",
+            "extra": "610581 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - B/op",
+            "value": 112,
+            "unit": "B/op",
+            "extra": "610581 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - allocs/op",
+            "value": 1,
+            "unit": "allocs/op",
+            "extra": "610581 times\n4 procs"
           }
         ]
       }
