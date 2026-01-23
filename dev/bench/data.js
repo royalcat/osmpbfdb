af097813305fb08b9d95a7401b2cf7fd777abe69
github-action-benchmark <github@users.noreply.github.com>
2026-01-23 04:27:21 +0000

    add Benchmark (go) benchmark result for a0b3706ba23b5c37780a33535dad774c03fe2f53

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1769142441728,
+  "repoUrl": "https://github.com/royalcat/osmpbfdb",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "committer": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "distinct": true,
+          "id": "a0b3706ba23b5c37780a33535dad774c03fe2f53",
+          "message": "change benchmark tool",
+          "timestamp": "2026-01-23T08:25:57+04:00",
+          "tree_id": "59db16fa31118695295b5e73a6f974c7cc3a6ed4",
+          "url": "https://github.com/royalcat/osmpbfdb/commit/a0b3706ba23b5c37780a33535dad774c03fe2f53"
+        },
+        "date": 1769142441462,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGet",
+            "value": 1695,
+            "unit": "ns/op",
+            "extra": "679814 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
