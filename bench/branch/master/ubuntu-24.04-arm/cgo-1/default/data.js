12d533f5548f36b3edeb1d017c9c3bc99fc93888
github-action-benchmark <github@users.noreply.github.com>
2026-01-23 10:25:14 +0000

    add Benchmark (go) benchmark result for c081976f4a52958aefb2580bb0568e897124f5da

---
+++ bench/branch/master/ubuntu-24.04-arm/cgo-1/default/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1769163914270,
+  "repoUrl": "https://github.com/royalcat/osmpbfdb",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "committer": {
+            "email": "k.adamovich20@gmail.com",
+            "name": "royalcat",
+            "username": "royalcat"
+          },
+          "distinct": true,
+          "id": "c081976f4a52958aefb2580bb0568e897124f5da",
+          "message": "bench fix branch",
+          "timestamp": "2026-01-23T14:23:04+04:00",
+          "tree_id": "fabe5fa70033cc80cf9fff590f51d589faed9376",
+          "url": "https://github.com/royalcat/osmpbfdb/commit/c081976f4a52958aefb2580bb0568e897124f5da"
+        },
+        "date": 1769163913980,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGet",
+            "value": 2019785,
+            "unit": "ns/op\t 4973346 B/op\t    8633 allocs/op",
+            "extra": "6247 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - ns/op",
+            "value": 2019785,
+            "unit": "ns/op",
+            "extra": "6247 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - B/op",
+            "value": 4973346,
+            "unit": "B/op",
+            "extra": "6247 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGet - allocs/op",
+            "value": 8633,
+            "unit": "allocs/op",
+            "extra": "6247 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
